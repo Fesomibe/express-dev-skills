27c595b0ceaf96452003e944a45fef167601b61a
Fesomibe <francisesomibe1@gmail.com>
2024-01-04 18:17:20 -0600

    Implementing the second request functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,8 +5,16 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create
   };
+
+  
+
+  function newSkill(req, res) {
+    res.render('skills/new', { title: 'New Skill' });
+  }
 
 function index(req, res) {
     res.render('skills/index', {
@@ -17,4 +25,12 @@ function index(req, res) {
 function show(req, res) {
     const skill = Skill.getOne(req.params.id)
     res.render('skills/show', { skill });
+}
+
+function create(req, res) {
+  console.log(req.body);
+  // The model is responsible for creating data
+  Skill.create(req.body);
+  // Do a redirect anytime data is changed
+  res.redirect('/skills');
 }
